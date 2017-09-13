df1f1c89976dc2d5ae58245896c485590abc9587
rlop666 <rlop666@gmail.com>
2017-09-13 09:11:51 +0200

    ejemplos

---
+++ tecnologias/empleado.ts
@@ -0,0 +1,12 @@
+export class Empleado {
+	nombre : string;
+	salario: number;
+	
+	constructor (pNombre, pSalario) { 
+		this.nombre = pNombre;
+		this.salario = pSalario;
+	}
+
+	getNombre () {}
+}
+© 2017 GitHub, Inc.
